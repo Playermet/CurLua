e2a702e706e9fb888a40a881b9bd48dd990e145e
Playermet <playermet@gmail.com>
2014-04-16 01:23:19 +0300

    Added more CURLINFO codes. Fix problem with NULL pointer in easy_getinfo

--- curl.lua
+++ curl.lua
@@ -38,6 +38,16 @@ ffi.cdef [[
 local lib = ffi.load 'libcurl.dll'
 
 -----------------------------------------------------------
+--  Type conversion
+-----------------------------------------------------------
+local type_converters = {
+  ['char*[1]']  = ffi.string;
+  ['long[1]']   = tonumber;
+  ['double[1]'] = tonumber;
+}
+
+
+-----------------------------------------------------------
 --  Codes
 -----------------------------------------------------------
 local option_codes = {
@@ -200,18 +210,51 @@ local option_utils = {
   end;
 }
 
-local info_funcs = {
-  response_code = function (handle)
-    local result = ffi.new('long[1]')
-    lib.curl_easy_getinfo(handle, 2097154, result)
-    return tonumber(result[0])
-  end;
+local info_data = {
+  effective_url           = {1048577, 'char*[1]'},
+  response_code           = {2097154, 'long[1]'},
+  http_connectcode        = {2097174, 'long[1]'},
+  filetime                = {2097166, 'long[1]'},
+  total_time              = {3145731, 'double[1]'},
+  namelookup_time         = {3145732, 'double[1]'},
+  connect_time            = {3145733, 'double[1]'},
+  appconnect_time         = {3145761, 'double[1]'},
+  pretransfer_time        = {3145734, 'double[1]'},
+  starttransfer_time      = {3145745, 'double[1]'},
+  redirect_time           = {3145747, 'double[1]'},
+  redirect_count          = {2097172, 'long[1]'},
+  redirect_url            = {1048607, 'char*[1]'},
+  size_upload             = {3145735, 'double[1]'},
+  size_download           = {3145736, 'double[1]'},
+  speed_download          = {3145737, 'double[1]'},
+  speed_upload            = {3145738, 'double[1]'},
+  header_size             = {2097163, 'long[1]'},
+  request_size            = {2097164, 'long[1]'},
+  ssl_verifyresult        = {2097165, 'long[1]'},
+  content_length_download = {3145743, 'double[1]'},
+  content_length_upload   = {3145744, 'double[1]'},
+  content_type            = {1048594, 'char*[1]'},
+  private                 = {1048597, 'char*[1]'},
+  httpauth_avail          = {2097175, 'long[1]'},
+  proxyauth_avail         = {2097176, 'long[1]'},
+  os_errno                = {2097177, 'long[1]'},
+  num_connects            = {2097178, 'long[1]'},
+  primary_ip              = {1048608, 'char*[1]'},
+  primary_port            = {2097192, 'long[1]'},
+  local_ip                = {1048617, 'char*[1]'},
+  local_port              = {2097194, 'long[1]'},
+  lastsocket              = {2097181, 'long[1]'},
+  ftp_entry_path          = {1048606, 'char*[1]'},
 
-  content_type = function (handle)
-    local result = ffi.new('char*[1]')
-    lib.curl_easy_getinfo(handle, 1048594, result)
-    return ffi.string(result[0])
-  end;
+  -- ssl_engines          = {4194331, 'curl_slist[1]'},
+  -- curlinfo_cookielist  = {4194332, 'curl_slist[1]'}
+  -- certinfo             = {4194338,'curl_certinfo[1]'}
+  -- tls_session          = {?, 'curl_tlsinfo[1]'}
+  -- condition_unmet      = {?, 'long[1]'},
+  -- rtsp_session_id      = {?, 'char*[1]'},
+  -- rtsp_client_cseq     = {?, 'long[1]'},
+  -- rtsp_server_cseq     = {?, 'long[1]'},
+  -- rtsp_cseq_recv       = {?, 'long[1]'},
 }
 
 -----------------------------------------------------------
@@ -254,10 +297,17 @@ local function easy_setopt(handle, key, val)
   return lib.curl_easy_setopt(handle, code, val)
 end
 
-local function easy_getinfo(handle, info)
-  local func = info_funcs[info]
-  if func then
-    return func(handle, info)
+local function easy_getinfo(handle, key)
+  local data = info_data[key]
+  if data then
+    local code = data[1]
+    local type = data[2]
+
+    local result = ffi.new(type)
+    lib.curl_easy_getinfo(handle, code, result)
+    if result[0] ~= nil then
+      return type_converters[type](result[0])
+    end
   end
 end
 
